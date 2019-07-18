77207fd5c1222eb2afbbb8a8d377e5f6eaf2abbf
Adidasik <43314978+akishinartem@users.noreply.github.com>
2019-07-18 15:05:46 +0300

    реализация защищенного класса

--- KarpLabaTP1/Protected.cpp
+++ KarpLabaTP1/Protected.cpp
@@ -1,4 +1,5 @@
 #include "Protected.h"
+
 #include <iostream>
 using namespace std;
 
@@ -88,7 +89,7 @@ void Protected::Copy(Protected& op1) {
 	delete[] Arr;
 }
 
-//	œŒ◊≈—¿“‹ œŒ–¿«Ã»Õ¿“‹
+//	√è√é√ó√Ö√ë√Ä√í√ú √è√é√ê√Ä√á√å√à√ç√Ä√í√ú
 void Protected::Calc() {
 	float harm = 0;
 	float del = 0;
